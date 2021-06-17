f43279aea4f26e796b60e7e17fc39fe228899a69
djtoohey <djtoohey@gmail.com>
2021-06-18 01:29:23 +0930

    added bullet spawning, need to still get them moving

--- Sechs_Krieg.py
+++ Sechs_Krieg.py
@@ -3,6 +3,8 @@
 # Imoprts
 import pygame
 import random
+from pygame import constants
+from pygame.constants import K_a, K_d, K_s, K_w
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -38,6 +40,9 @@ class Player(pygame.sprite.Sprite):
     # movements
     # checks for key press, moves according to players movement speed
     def update(self, pressed_keys):
+        centerOfPlayerX = (self.rect.left + self.rect.right)/2
+        centerOfPlayerY = (self.rect.top + self.rect.bottom)/2
+
         # up
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -(self.movementSpeed))
@@ -50,7 +55,20 @@ class Player(pygame.sprite.Sprite):
         # right
         if pressed_keys[K_RIGHT]:
             self.rect.move_ip((self.movementSpeed), 0)
-
+        
+        if pressed_keys[K_w]:
+            print("x", centerOfPlayerX, ", y", centerOfPlayerY)
+            bullet = Bullet(centerOfPlayerX, centerOfPlayerY, (255, 0, 0), -BULLET_SPEED)
+            all_sprites.add(bullet)
+        if pressed_keys[K_s]:
+            bullet = Bullet(centerOfPlayerX, centerOfPlayerY, (0, 255, 0), BULLET_SPEED)
+            all_sprites.add(bullet)
+        if pressed_keys[K_a]:
+            bullet = Bullet(centerOfPlayerX, centerOfPlayerY, (0, 0, 255), BULLET_SPEED)
+            all_sprites.add(bullet)
+        if pressed_keys[K_d]:
+            bullet = Bullet(centerOfPlayerX, centerOfPlayerY, (255, 0, 255), BULLET_SPEED)
+            all_sprites.add(bullet)
         # checking that the player cannot escape the display borders
         # left border
         if self.rect.left < 0:
@@ -65,6 +83,34 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom >= HEIGHT:
             self.rect.bottom = HEIGHT
             
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, spawnPosX, spawnPosY, color, speed):
+        super(Bullet, self).__init__()
+        self.surf = pygame.Surface((20,10))
+        self.surf.fill(color)
+        self.rect = self.surf.get_rect(
+            center=(
+                spawnPosX,
+                spawnPosY
+            )
+        )
+
+        self.speed = speed
+
+        def update(self):
+            # kills the bullet after it has left the screen
+            # left border
+            if self.rect.left < 0:
+                self.kill()
+            # right border
+            if self.rect.right > WIDTH:
+                self.kill()
+            # top border
+            if self.rect.top <= 0:
+                self.kill()
+            # bottom border
+            if self.rect.bottom >= HEIGHT:
+                self.kill()
 
 # WIP
 # CLASS: Enemy
@@ -90,7 +136,7 @@ class Enemy(pygame.sprite.Sprite):
 
 # declaring the width and height of the display
 WIDTH, HEIGHT = 800, 800
-
+BULLET_SPEED = 2
 # setting the width and height
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -121,10 +167,10 @@ while running:
         elif event.type == QUIT:
             running = False
         # creating a new enemy and adding it to the enemies and all_sprites sprite groups
-        elif event.type == ADDENEMY:
-            new_enemy = Enemy()
-            enemies.add(new_enemy)
-            all_sprites.add(new_enemy)
+        # elif event.type == ADDENEMY:
+        #     new_enemy = Enemy()
+        #     enemies.add(new_enemy)
+        #     all_sprites.add(new_enemy)
         # if playerhp is <= 0, game over
         elif player.hp <=0:
             running = False
